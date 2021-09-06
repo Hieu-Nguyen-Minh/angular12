6c0f3b47f43f35be77e94d1b93ffff4302316c23
Hieu Nguyen <minhhieun000@gmail.com>
2021-09-06 14:43:05 +0700

    [NMH] kết nối dữ liệu từ component ra HTML

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'hieu nguyen'; // đẩy data từ component ra HTML hay còn gọi là databinding value 1 chiều
   // đẩy data từ component ra thẻ HTML thông qua thuộc tính của thẻ
+  public age = 18;
   constructor() {}
 
   ngOnInit(): void {}
