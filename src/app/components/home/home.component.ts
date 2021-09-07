78b477f180ca9c801ce621c3dba10898c10152ab
Hieu Nguyen <minhhieun000@gmail.com>
2021-09-07 08:54:03 +0700

    [NMH] Cascading ComboBox. Viết danh sách chọn tỉnh/thành rồi quận/huyện

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,15 +10,100 @@ export class HomeComponent implements OnInit {
   // đẩy data từ component ra thẻ HTML thông qua thuộc tính của thẻ
   public age = 16;
 
-  public traicay = [
-    { name: 'TÁO', price: 99, hagia: true },
-    { name: 'Nho', price: 99, hagia: false },
-    { name: 'LÊ', price: 45 },
-    { name: 'Chuối', price: -6, hagia: true },
+  public distincts: string[] | string = ['quận huyện'];
+
+  public vietNamData = [
+    { city: 'Chọn thành phố', district: 'quận huyện' },
+    {
+      city: 'An Giang',
+      district: [
+        'Thành phố Long Xuyên',
+        'Thành phố Châu Đốc',
+        'Thị xã Tân Châu',
+        'Huyện An Phú',
+        'Huyện Châu Phú',
+        'Huyện Châu Thành',
+        'Huyện Chợ Mới',
+        'Huyện Phú Tân',
+        'Huyện Thoại Sơn',
+        'Huyện Tịnh Biên',
+        'Huyện Tri Tôn',
+      ],
+    },
+    {
+      city: 'Bà Rịa - Vũng Tàu',
+      district: [
+        'Thành phố Vũng Tàu',
+        'Thị xã Bà Rịa',
+        'Thị xã Phú Mỹ',
+        'Huyện Châu Đức',
+        'Huyện Côn Đảo',
+        'Huyện Đất Đỏ',
+        'Huyện Long Điền',
+        'Huyện Tân Thành',
+        'Huyện Xuyên Mộc',
+      ],
+    },
+    {
+      city: 'Hà Nội',
+      district: [
+        'Quận Ba Đình',
+        'Huyện Ba Vì',
+        'Quận Bắc Từ Liêm',
+        'Quận Cầu Giấy',
+        'Huyện Chương Mỹ',
+        'Huyện Đan Phượng',
+        'Huyện Đông Anh',
+        'Quận Đống Đa',
+        'Huyện Gia Lâm',
+        'Quận Hà Đông',
+        'Quận Hai Bà Trưng',
+        'Huyện Hoài Đức',
+        'Quận Hoàn Kiếm',
+        'Quận Hoàng Mai',
+        'Quận Long Biên',
+        'Huyện Mê Linh',
+        'Huyện Mỹ Đức',
+        'Quận Nam Từ Liêm',
+        'Huyện Phú Xuyên',
+        'Huyện Phúc Thọ',
+        'Huyện Quốc Oai',
+        'Huyện Sóc Sơn',
+        'Thị xã Sơn Tây',
+        'Quận Tây Hồ',
+        'Huyện Thạch Thất',
+        'Huyện Thanh Oai',
+        'Huyện Thanh Trì',
+        'Quận Thanh Xuân',
+        'Huyện Thường Tín',
+        'Huyện Ứng Hòa',
+      ],
+    },
   ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  changeCity(e: any) {
+    const city = e.target.value;
+
+    if (!city) {
+      return;
+    }
+
+    // cách 1
+    // const search = this.vietNamData.filter((data) => data.city === city); // trả về 1 mảng mới chứa các obj thoả mãn đk
+    // if (search && search.length > 0) {
+    //   this.distincts = search[0].district;
+    //   console.log(this.distincts);
+    // }
+
+    //cách 2
+
+    this.distincts =
+      this.vietNamData.find((d) => d.city === city)?.district || []; //tìm trong vietNamData có thằng nào thoản mãn đk
+    // thì trả về district của thằng đó nếu không có trả về mảng rỗng
+  }
 
   resetName() {
     this.name = '';
