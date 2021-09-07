6760cb8d56c7fc041cf54af8389c319f9e516374
Hieu Nguyen <minhhieun000@gmail.com>
2021-09-07 11:40:44 +0700

    [NMH] ngSwitch dùng cho nhiều lựa chọn trong HTML

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  public login = 'user';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  changeLoginAdmin() {
+    this.login = 'admin';
   }
 
+  changeLoginUser() {
+    this.login = 'user';
+  }
 }
