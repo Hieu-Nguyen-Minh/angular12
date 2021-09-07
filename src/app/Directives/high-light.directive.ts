78f27edce29f3a395677a48761d05c34b487edad
Hieu Nguyen <minhhieun000@gmail.com>
2021-09-07 15:27:08 +0700

    [NMH] Directives. Component, Structural, Attribute. Cách tạo + xài Directive riêng

--- src/app/Directives/high-light.directive.ts
+++ src/app/Directives/high-light.directive.ts
@@ -6,6 +6,13 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class HighLightDirective implements OnInit {
   @Input('appHighLight') appHighLightc = 'blue';
 
+  // directives là các class bổ xung hành vi cho các phần tử trong app
+  // có 3 loại directive là :
+  // components
+  // Attribute directives dùng để thay đổi giao diện hoặc hành vi của các phần tử trong DOM
+  // Structural directives dùng để thay đổi bố cục DOM = cách thêm hoặc xoá các ptử trong DOM,
+  // ví dụ : ngIf,ngFor,...
+
   constructor(private el: ElementRef) {
     el.nativeElement.style.color = this.appHighLightc;
     // console.log('appHighLight contructor', this.appHighLightc);
